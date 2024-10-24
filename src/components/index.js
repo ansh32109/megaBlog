c931e85771ac1086318d8f614b7b5e511a2271cc
Ainesh Sinha <studentainesh@gmail.com>
2024-10-25 01:04:53 +0530

    Added form and slug

--- src/components/index.js
+++ src/components/index.js
@@ -3,5 +3,9 @@ import Footer from "./Footer/Footer";
 import Container from './container/Container'
 import Logo from "./Logo";
 import LogoutBtn from "./Header/LogoutBtn";
+import RTE from './RTE';
+import Signup from './Signup'
+import Button from './Button'
+import Input from './Input'
 
-export {Header, Footer, Container, Logo, LogoutBtn};
+export {Header, Footer, Container, Logo, LogoutBtn, RTE, Signup, Button, Input};
